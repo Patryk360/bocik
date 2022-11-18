24f221a7431d89b945835b700547de7b120d2958
The_Kruku <46572994+Kruku2006@users.noreply.github.com>
2022-11-18 22:51:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     dbd_license: '8464573d-b798-41bf-b369-7c2c26aebf57',
     redirect_uri: 'http://localhost/discord/callback',
     discord: {
-        bot_token: '8464573d-b798-41bf-b369-7c2c26aebf57',
-        client_id: '962324941406076938',
-        client_secret: 'PehaWmLYF03zEnagHhJmO9rYsfEYugAO',
+        bot_token: 'token',
+        client_id: 'ds',
+        client_secret: 'P',
     },
 };
